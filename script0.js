f3393da8ce7baa1532ab58f2aaef2ee4cdd1bc17
ksuch16 <ksuch16070@gmail.com>
2024-10-23 12:11:37 -0500

    Update script0.js

--- script0.js
+++ script0.js
@@ -1,1 +1,10 @@
 
+function fetchData() {
+  event.preventDefault();
+  var startdate = document.getElementById("startdate").value;
+  var enddate = document.getElementById("enddate").value;
+  console.log("Starting date: " + startdate);
+  console.log("Ending date: " + enddate);
+  fetch("https://compute.samford.edu/zohauth/clients/data");
+}
+
